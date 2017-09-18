626899d9a3adc48ecb699670bab2bbe19d640154
Denis Y. Spichkin <ds@levellab.ru>
2017-09-18 13:23:57 +0300

    fix

--- djangoapps/peerjs/views.py
+++ djangoapps/peerjs/views.py
@@ -5,9 +5,10 @@ import json
 
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 from djwebrtc.redis_utils import set_clients, get_clients
-
+from accounts.models import Account
 # from rest_framework.decorators import api_view, permission_classes
 # from rest_framework.response import Response
 # from rest_framework import status
@@ -18,13 +19,18 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 
 def get_id(request, key):
+    user = get_object_or_404(Account, key=key)
     hash_object = hashlib.sha256(key)
     hex_dig = hash_object.hexdigest()[:15]
+    if user.key_id != hex_dig:
+        user.key_id = hex_dig
+        user.save()
     return HttpResponse(hex_dig)
 
 
 @csrf_exempt
 def set_streaming(request, key, id, token):
+    """
     ip = request.META['REMOTE_ADDR']
 
     clients = get_clients()
@@ -39,7 +45,7 @@ def set_streaming(request, key, id, token):
     else:
         set_clients(clients)
         # start_streaming(clients, key, id, token)
-
+    """
     pad = '00'
     for i in range(0, 10):
         pad += pad
