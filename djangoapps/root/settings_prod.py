69fe6568bd38cda781f49efe331618aba81286a3
Denis Y. Spichkin <ds@levellab.ru>
2017-10-12 19:19:14 +0300

    fix

---
+++ djangoapps/root/settings_prod.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from root.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+BASE_URL = 'https://www.mydialogs.ru/'
+
+ALLOWED_HOSTS = ['*']
+
+ADMINS = (
+    ('Denis Spichkin', 'user783@gmail.ru'),
+)
+
+EMAIL_SUBJECT_PREFIX = '[mydialogs.ru] '
+
+SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = False
