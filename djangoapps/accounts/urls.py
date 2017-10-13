a507e5660bd3076901c576adcef015ea00641f23
Denis Y. Spichkin <ds@levellab.ru>
2017-10-13 16:00:07 +0300

    fix

--- djangoapps/accounts/urls.py
+++ djangoapps/accounts/urls.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from django.conf.urls import include, url
-
-from peerjs.views import (
-    get_id, set_streaming, handle
-)
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
 
 from accounts.views import (
     accept_call, message, check_user, registration, confirm_email,
     remember, user
     )
 
+
+def if_authentificated_redirect(function):
+    def wrap(request, *args, **kwargs):
+        if request.user and request.user.is_authenticated:
+            return HttpResponseRedirect('/')
+        else:
+            return function(request, *args, **kwargs)
+    return wrap
+
 urlpatterns = [
+    url(r'^login/?', if_authentificated_redirect(LoginView.as_view()), name='login'),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^', include('allauth.urls')),
     url(r'^registration/', registration, name="registration"),
