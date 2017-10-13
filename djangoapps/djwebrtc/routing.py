386fe4b707e59e88d4f9ae176b8c9e1f0e2a4047
Denis Y. Spichkin <ds@levellab.ru>
2017-10-13 13:52:07 +0300

    fix

--- djangoapps/djwebrtc/routing.py
+++ djangoapps/djwebrtc/routing.py
@@ -5,7 +5,11 @@ from djwebrtc.consumers import (
 
 
 channel_routing = [
-    route('websocket.connect', ws_connect, path=r"^/peerjs/?$"),
-    route('websocket.receive', ws_message, path=r"^/peerjs/?$"),
-    route('websocket.disconnect', ws_disconnect, path=r"^/peerjs/?$"),
+    #route('websocket.connect', ws_connect, path=r"^/peerjs/?$"),
+    #route('websocket.receive', ws_message, path=r"^/peerjs/?$"),
+    #route('websocket.disconnect', ws_disconnect, path=r"^/peerjs/?$"),
+
+    route('websocket.connect', ws_connect),
+    route('websocket.receive', ws_message),
+    route('websocket.disconnect', ws_disconnect),
 ]
