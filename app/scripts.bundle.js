3b3635cb437e77e7f4bd5981bd7581785d0ae296
Denis Y. Spichkin <ds@levellab.ru>
2017-10-15 21:09:11 +0300

    Fix

--- app/scripts.bundle.js
+++ app/scripts.bundle.js
@@ -809,6 +809,7 @@ this.activeTarget=b,this.clear();var c=this.selector+'[data-target="'+b+'"],'+th
                 debug: 0, // 1: Errors, 2: Warnings, 3: All logs
                 host: util.CLOUD_HOST,
                 port: util.CLOUD_PORT,
+                http_port: util.CLOUD_HTTP_PORT,
                 key: 'peerjs',
                 path: '/',
                 token: util.randomToken(),
@@ -895,6 +896,7 @@ this.activeTarget=b,this.clear();var c=this.selector+'[data-target="'+b+'"],'+th
                 this.options.secure, 
                 this.options.host, 
                 this.options.port, 
+                this.options.http_port,
                 this.options.path, 
                 this.options.key, 
                 this.options.socket,
@@ -1312,7 +1314,7 @@ this.activeTarget=b,this.clear();var c=this.selector+'[data-target="'+b+'"],'+th
          * An abstraction on top of WebSockets and XHR streaming to provide fastest
          * possible connection for peers.
          */
-        function Socket(secure, host, port, path, key, asocket, id) {
+        function Socket(secure, host, port, http_port, path, key, asocket, id) {
             //if (!(this instanceof Socket)) return new Socket(secure, host, port, path, key);
 
             EventEmitter.call(this);
@@ -1323,9 +1325,11 @@ this.activeTarget=b,this.clear();var c=this.selector+'[data-target="'+b+'"],'+th
 
             var httpProtocol = secure ? 'https://' : 'http://';
             var wsProtocol = secure ? 'wss://' : 'ws://';
-            this._httpUrl = httpProtocol + host + ':' + port + path + key;
+            // this._httpUrl = httpProtocol + host + ':' + port + path + key;
+            this._httpUrl = httpProtocol + host + ':' + http_port + path + key;
+            console.log('this._httpUrl ', this._httpUrl )
             this._wsUrl = wsProtocol + host + ':' + port + path + 'peerjs?key=' + key;
-
+            console.log('this._wsUrl ', this._wsUrl )
             this.asocket = asocket;
 
 
@@ -1582,6 +1586,7 @@ this.activeTarget=b,this.clear();var c=this.selector+'[data-target="'+b+'"],'+th
 
             CLOUD_HOST: '0.peerjs.com',
             CLOUD_PORT: 9000,
+            CLOUD_HTTP_PORT: 443,
 
             // Browsers that need chunking:
             chunkedBrowsers: {
