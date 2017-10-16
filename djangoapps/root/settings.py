667aed1302dcbd42ef6bfdf1560552f53c7ea269
Denis Y. Spichkin <ds@levellab.ru>
2017-10-16 21:53:33 +0300

    fix

--- djangoapps/root/settings.py
+++ djangoapps/root/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -288,6 +289,10 @@ LOGGING = {
             'formatter': 'default',
             'filename': os.path.join(LOGS_ROOT, 'debug.log')
         },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        }
     },
     'loggers': {
         'django.request': {
